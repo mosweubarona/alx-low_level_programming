a6af801cef45c32375f00090b71cd7399f7ccc72
mosweubarona <mosweubarona@gmail.com>
2022-03-10 09:06:39 -0800

    fixed comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/*this is comment, print that and return zero*/
+
+/**
+ * main - Entry first
+ * Return: Always 0 success
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
