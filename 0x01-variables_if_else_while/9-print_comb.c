3121a7aedb8d76a246c9869134a8790e78938954
mosweubarona <mosweubarona@gmail.com>
2022-03-11 09:01:03 -0800

    my commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print possible combinations
+ * Return: alwayss 0 success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
