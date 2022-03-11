64a6a9581016d23a2ce2d083e82081eb03427d87
mosweubarona <mosweubarona@gmail.com>
2022-03-11 07:35:36 -0800

    my commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet excluding q and e
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
