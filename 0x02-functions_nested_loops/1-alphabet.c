817c9a789c76d6244f14266e4bd9f0a717eab53e
mosweubarona <mosweubarona@gmail.com>
2022-03-15 08:36:02 -0700

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets in lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
