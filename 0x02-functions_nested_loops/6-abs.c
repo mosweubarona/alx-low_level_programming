ba4e34186f8e1f70726fbdeccad7bd08a291b2d9
mosweubarona <mosweubarona@gmail.com>
2022-03-15 11:03:12 -0700

    my commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - integer value
+ * @n: the number to check
+ * Return: the value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
