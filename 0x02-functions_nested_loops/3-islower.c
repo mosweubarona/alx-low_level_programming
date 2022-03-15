2fcd976e5b4df6311feb89cb4c27b22416bec083
mosweubarona <mosweubarona@gmail.com>
2022-03-15 09:34:41 -0700

    my commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - shows lower cases
+ * @c: the checked charector
+ * Return: 1 if c is lower case
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
