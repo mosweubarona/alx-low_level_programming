c902ad9d7d2962976fed86e176fd085b074159cb
mosweubarona <mosweubarona@gmail.com>
2022-03-17 03:52:21 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case
+ * @c : charector to cheak
+ * Return: print the number
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
