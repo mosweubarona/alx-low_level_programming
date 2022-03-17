d2fc24a33b80e50a7fca8323323799f7689ff0f5
mosweubarona <mosweubarona@gmail.com>
2022-03-17 12:17:02 -0700

    my commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test for negative of positive
+ * Return: the found number
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
