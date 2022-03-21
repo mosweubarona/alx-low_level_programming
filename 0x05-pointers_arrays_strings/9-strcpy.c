f2531ac6c6f6da90fb750233e30ad8fb61ed7db9
mosweubarona <mosweubarona@gmail.com>
2022-03-21 14:10:04 -0700

    my commit 9strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string
+ * @dest: string
+ * @src: string
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
