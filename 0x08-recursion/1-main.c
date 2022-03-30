20e3adb9b7384c64ebda0cc0cecf0777a47d9d95
mosweubaronavb <mosweubarona@gmail.com>
2022-03-30 15:21:58 +0000

    my commit 2str

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	_print_rev_recursion("\nColton Walker");
+	return (0);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - lengthh of string
+ * @s: string to check
+ * Return: the length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
