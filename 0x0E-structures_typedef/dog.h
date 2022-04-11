c6082b3f5b64323f532d54c0e38642d6b3e3d80a
mosweubaronavb <mosweubarona@gmail.com>
2022-04-11 17:45:52 +0000

    my commit first

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct def of a dog
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ * Description: Longer dscription
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /*DOG_H*/
