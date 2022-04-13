4c1f9397e5e8e794a890547b289bf0e75fa4441c
mosweubaronavb <mosweubarona@gmail.com>
2022-04-13 12:55:39 +0000

    my commit fix 1 arry

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,15 +4,15 @@
  * array_iterator - executes a fucntion given as a parameter
  * @array: array to iterate
  * @size: array size
- * @size_t: array size
  * @action: pointer to function
  */
-void array_iterator(int *array, size_t, size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!array || !action)
-		retun;
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
