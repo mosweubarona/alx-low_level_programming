ff176f95dcea0c82d2c6a0df7c4a01938d5f2fa9
mosweubaronavb <mosweubarona@gmail.com>
2022-04-14 10:50:45 +0000

    my commit first

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers with separators
+ * @separator: separate numbers
+ * @n: numbers of integers passed to function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", va_arg(list, int));
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(list);
+
+	printf("\n");
+}
