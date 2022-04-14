df3af30ddd9c74ac83817efe946a071684fb9d0b
mosweubaronavb <mosweubarona@gmail.com>
2022-04-14 11:14:10 +0000

    my commit 2print fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
-	va_end(list);
 
-	printf("\n");
+	putchar("\n");
+
+	va_end(list);
 }
