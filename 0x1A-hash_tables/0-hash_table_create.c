309b7fef16c52407e7fe02dd667c7169a719a852
mosweubarona <mosweubarona@gmail.com>
2022-07-01 11:11:59 -0700

    my commit fix 0hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,29 +2,25 @@
 
 /**
  * hash_table_create - creates a hash table
- * @size: size of the table
- * Return: hash table on success, NULL on failure.
+ * @size: size of the array of the hash table
+ *
+ * Return: Pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-
 	hash_table_t *hash_table;
 
 	if (size == 0)
 		return (NULL);
 
 	hash_table = malloc(sizeof(hash_table_t));
-
-	if (hash_table == NULL)
+	if (!hash_table)
 		return (NULL);
 
 	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	hash_table->array = calloc((size_t)hash_table->size, sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
-	{
-		free(hash_table);
 		return (NULL);
-	}
-	return (hash_table);
 
+	return (hash_table);
 }
